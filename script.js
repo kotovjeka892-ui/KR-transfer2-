4500ee8ced5717d30405df9e5a675bbff173990d
kotovjeka892-ui <kotovjeka892@gmail.com>
2026-08-14 14:32:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function choose(route){document.getElementById('route').value=route;document.getElementById('booking').scrollIntoView({behavior:'smooth'});}
+function submitForm(e){e.preventDefault();alert('Демо: форма працює. На наступному кроці підключимо реальні заявки в Telegram.');}
